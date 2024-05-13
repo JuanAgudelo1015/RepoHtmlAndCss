f6293a0aa96d3f15d176606755e759e01fd87e4b
Juan Agudelo <agudelojuanpablo398@gmail.com>
2024-05-13 13:27:19 -0500

    Section featured finished

--- destacados/test/java.js
+++ destacados/test/java.js
@@ -1,19 +1,36 @@
+/**Paremetros del metodo addEventListener
+ * - tipo de evento
+ * - funcion ah ejecutar
+ * - captura
+ * Primero se esta verificando que el dom haya sido cargado para ejecutar
+ * la funcion
+ */
 document.addEventListener('DOMContentLoaded', function () {
   const tarjetasPrincipales = document.querySelectorAll('.main');
-
+  /**En el primer forEach se esta iterando sobre todas las tarjetas tarjetasPrincipales
+   * y se les esta asignando un escuchador de eventos
+   */
   tarjetasPrincipales.forEach(tarjeta => {
+    // asignando el evento de click
     tarjeta.addEventListener('click', () => {
+      //Vuelve y recorre el forEach de tarjetas principales es decir en las que no se hicieron click
+      // y luego compara si la en las tarjetas que no se hicieron click es diferente de la que hizo click
+      // y si es diferente las oculta
       tarjetasPrincipales.forEach(e => {
         if (e !== tarjeta) {
           e.querySelector('.more-information').style.display = 'none';
         }
       });
       const detalles = tarjeta.querySelector('.more-information');
-      if (detalles.style.display === 'block') {
+      // Se esta verificando si el estilo de elemento actual es estrictamente igual ah bloque,
+      // si esta condicion se cumple oculta todas las tarjetas
+      if(detalles.style.display === 'block') {
         detalles.style.display = 'none';
-      } else {
+      } 
+      // Si los detalles estan actulmente ocultos (none) se cambia el estilo de visualizacion ah bloque
+      else {
         detalles.style.display = 'block';
-      }
+      } 
     });
   });
 });
